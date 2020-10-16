f1fb6ce53e4945c2f8e01d0cdf5e7a59e1e9e8c1
RainbowForest <rainbowforestgit@gmail.com>
2020-10-16 19:04:30 +0200

    Added HeaderGenerator class

---
+++ product-catalog-service/src/main/java/com/rainbowforest/productcatalogservice/http/header/HeaderGenerator.java
@@ -0,0 +1,36 @@
+package com.rainbowforest.productcatalogservice.http.header;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HeaderGenerator {
+	
+	public HttpHeaders getHeadersForSuccessGetMethod() {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("Content-Type", "application/json; charset=UTF-8");
+		return httpHeaders;
+	}
+	
+	public HttpHeaders getHeadersForError() {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("Content-Type", "application/problem+json; charset=UTF-8");
+		return httpHeaders;
+	}
+	
+	public HttpHeaders getHeadersForSuccessPostMethod(HttpServletRequest request, Long newResourceId) {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		try {
+			httpHeaders.setLocation(new URI(request.getRequestURI() + "/" + newResourceId));
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		httpHeaders.add("Content-Type", "application/json; charset=UTF-8");
+		return httpHeaders;
+	}
+}
